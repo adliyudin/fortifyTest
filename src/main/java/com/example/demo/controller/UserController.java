837b30c6ba565bfcf24932405787bc9fc1742d0a
administrator <aadaliyudin12@gmail.com>
2026-05-06 08:07:15 +0700

    Initial Commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,21 @@
+﻿package com.example.demo.controller;
+
+import com.example.demo.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(""/user"")
+public class UserController {
+
+    private UserService userService = new UserService();
+
+    @GetMapping(""/get"")
+    public String getUser(@RequestParam String username) {
+        return userService.getUserByUsername(username);
+    }
+
+    @GetMapping(""/hello"")
+    public String hello(@RequestParam String name) {
+        return ""<h1>Hello "" + name + ""</h1>"";
+    }
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,30 @@
+﻿package com.example.demo.service;
+
+import java.sql.*;
+
+public class UserService {
+
+    private String dbUrl = ""jdbc:mysql://localhost:3306/test"";
+    private String dbUser = ""root"";
+    private String dbPass = ""password"";
+
+    public String getUserByUsername(String username) {
+        String result = """";
+        try {
+            Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPass);
+            Statement stmt = conn.createStatement();
+
+            String query = ""SELECT * FROM users WHERE username = '"" + username + ""'"";
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                result = rs.getString(""username"");
+            }
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
